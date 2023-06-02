7cbda9b596da494a1280ce1ba210e42cba2d5dba
toshiko1045 <104748158+toshiko1045@users.noreply.github.com>
2023-06-02 13:57:55 +0000

    firstspaces

--- test.cpp
+++ test.cpp
@@ -2,26 +2,7 @@
 #include <vector>
 using namespace std;
 
-void enumerateStrings(int length, string current) {
-    // Base case: If the current string has the desired length, print it
-    if (current.length() == length) {
-        cout << current << endl;
-        return;
-    }
-
-    // Recursive case: Add a, b, or c to the current string and call the function recursively
-    enumerateStrings(length, current + 'a');
-    enumerateStrings(length, current + 'b');
-    enumerateStrings(length, current + 'c');
-}
-
-int main() {
-    int length;
-    cout << "Enter the length of the strings: ";
-    cin >> length;
-
-    cout << "Enumerated strings:\n";
-    enumerateStrings(length, "");
-
+int main(){
+    cout << "Hello, git code space" << endl;
     return 0;
 }
