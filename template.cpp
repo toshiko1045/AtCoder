ed889bcd44d9c35616c15a421a63807c2f931033
toshiko1045 <104748158+toshiko1045@users.noreply.github.com>
2023-12-08 15:00:21 +0000

    did AtCoder

--- template.cpp
+++ template.cpp
@@ -11,6 +11,8 @@ template<class T, class... A> void print(const T& first, const A&... rest);
 template<class T, class... A> void println(const T& first, const A&... rest);
 template<class T> void print(const vector<T>& vec){print("[ ");for(T v:vec) print(v," ");print("]");}
 template<class T> void println(const vector<T>& vec){print("[ ");for(T v:vec) print(v," ");println("]");}
+template<class T, class U> void print(const pair<T,U>& p){print("( ", p.first, " ", p.second, " )");}
+template<class T, class U> void println(const pair<T,U>& p){println("( ", p.first, " ", p.second, " )");}
 template<class T, class... A> void print(const T& first, const A&... rest){print(first);print(rest...);}
 template<class T, class... A> void println(const T& first, const A&... rest){print(first);println(rest...);}
 
@@ -21,22 +23,13 @@ int main(){
     println("a");
     println(1);
 
-    // print(1,"A", 2, "\n");
-    // print('A', "A", 1, 2.45, "\n");
-
-    vector<int> v(5);
-    for(int i=0;i<v.size();i++) v.at(i) = i;
-    // for(int i : v) print(i);
-    print(v, "a\n");
-    println(v,"a");
-
     vector<vector<int>> v2(5, vector<int>(3));
     for(int i=0;i<v2.size();i++) for(int j=0;j<v2.at(i).size();j++) v2.at(i).at(j) = i+j;
     print(v2, "\n");
     println(v2) ;
 
-    int a = 12;
-    println(a);
+    pair<string, int> p("hoge", 234);
+    print(p);
 
     return 0;
 }
